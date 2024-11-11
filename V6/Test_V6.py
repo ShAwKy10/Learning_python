64dcc53a89c144d4ea43114a0b4d51836dfc56e5
Shawky <amamasa121212@gmail.com>
2024-11-11 19:15:47 +0200

    The playground file of video 6

---
+++ V6/Test_V6.py
@@ -0,0 +1,83 @@
+# if True:
+#     print('Conditional Was True')
+
+# if False:
+#     print('Conditional Was True')
+
+
+
+
+
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is
+
+# language = 'Java'
+
+# if language == 'Python':
+#     print('Language is Python')
+# elif language == 'Java':
+#     print('Language is Java')
+# elif language == 'JavaScript':
+#     print('Language is JavaScript')
+# else:
+#     print('No match')
+
+
+
+
+
+# and
+# or
+# not
+
+# user = 'Admain'
+# logged_in = False
+
+# if user == 'Admain' and logged_in:
+#     print('Admain page')
+# else:
+#     print('Bad Creds')
+
+# if user == 'Admain' or logged_in:
+#     print('Admain page')
+# else:
+#     print('Bad Creds')
+
+# if not logged_in:
+#     print('Please Log In')
+# else:
+#     print('Welcome')
+
+
+
+
+
+# a = [1, 2, 3]
+# b = a
+
+# print(a == b)
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequance. For example, '', (), [].
+    # Any empty mapping. For example, {}.
+
+condition = 0
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
